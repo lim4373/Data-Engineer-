1ebeb83a7a7ca0ed60e138e6c6dba510a8d780e1
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-12-04 02:11:49 +0900

    Update download-data.sh

--- jobs/download-data.sh
+++ jobs/download-data.sh
@@ -3,14 +3,13 @@
 DATA_DIR=/opt/bitnami/spark/data
 
 API_KEY=""
-CHANNEL_ID="UCMki_UkHb4qSc0qyEcOHHJw"
-TARGET_DATE=$1  # Airflow에서 전달받는 날짜 (yyyy-mm-dd 형식)
+CHANNEL_ID=""
+TARGET_DATE=$1  
 DATA_DIR="/opt/bitnami/spark/data"
 OUTPUT_PATH="${DATA_DIR}/${TARGET_DATE}.json"
 
 echo "Fetching YouTube Data for date: $TARGET_DATE..."
 
-# Python 스크립트를 실행하여 YouTube 데이터를 가져옵니다.
 python3 /opt/airflow/jobs/fetch_youtube_data.py \
   --api_key "${API_KEY}" \
   --channel_id "${CHANNEL_ID}" \
